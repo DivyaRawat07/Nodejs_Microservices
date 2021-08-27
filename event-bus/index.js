5adaf16ab2f59b70d4d0a2acb51eaeb30e55139e
DivyaRawat07 <Divyarawat@rapidinnovation.dev>
2021-08-27 19:30:26 +0530

    'changes'

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,9 +8,10 @@ app.use(bodyParser.json())
 app.post('/events',(req, res)=>{
     const events = req.body
 
-    axios.post('http://localhost:4000/events',event)
+    axios.post('http://localhost:8000/events',event)
     axios.post('http://localhost:4001/events',event)
     axios.post('http://localhost:4002/events',event)
+    axios.post('http://localhost:4003/events',event)
 
     res.send({status:'OK'})
 
